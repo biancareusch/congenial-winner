07e0aff83a596895651bac8335afa874cde5436b
Bianca Reusch <reusch.bianca@gmail.com>
2021-02-02 09:52:42 -0600

    added debug logger for springframework

--- core/src/main/java/spring/five/Main.java
+++ core/src/main/java/spring/five/Main.java
@@ -2,6 +2,8 @@ package spring.five;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -11,5 +13,18 @@ public class Main {
     public static void main(String[] args) {
         log.info("Guess The Number");
 
+        //create context (container)
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+        //call method next() to get a random number
+        int number = numberGenerator.next();
+
+        //log generated number
+        log.info("number = {}", number);
+
+        //close context (container)
+        context.close();
     }
 }
