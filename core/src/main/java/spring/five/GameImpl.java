8e2f8290519598df4e63bfe0abb853496e7041bb
Bianca Reusch <reusch.bianca@gmail.com>
2021-02-09 09:49:12 -0600

    added javax annotation dependencies

--- core/src/main/java/spring/five/GameImpl.java
+++ core/src/main/java/spring/five/GameImpl.java
@@ -3,6 +3,8 @@ package spring.five;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+
 public class GameImpl implements Game{
 
     // == constants ==
