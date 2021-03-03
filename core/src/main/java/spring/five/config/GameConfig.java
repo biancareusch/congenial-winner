82f4b51c3afd055c1ab4b9423cd14f204fd1296b
Bianca Reusch <reusch.bianca@gmail.com>
2021-03-03 13:36:03 -0600

    added game.properties

--- core/src/main/java/spring/five/config/GameConfig.java
+++ core/src/main/java/spring/five/config/GameConfig.java
@@ -1,16 +1,23 @@
 package spring.five.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import spring.five.GuessCount;
 import spring.five.MaxNumber;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 50;
-    private int guessCount = 8;
+    @Value("${game.maxNumber:20}") //added default value if game.properties can't be found
+    private int maxNumber;
+
+    @Value("${game.guessCount:5}")
+    private int guessCount;
+
     // == bean methods ==
     @Bean
     @MaxNumber
