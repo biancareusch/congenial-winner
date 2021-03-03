03e82933115ddac67338f7496f5b454be0b2f686
Bianca Reusch <reusch.bianca@gmail.com>
2021-03-03 12:17:19 -0600

    moved main to a console module

--- console/src/main/java/spring/five/console/Main.java
+++ console/src/main/java/spring/five/console/Main.java
@@ -1,10 +1,13 @@
-package spring.five;
+package spring.five.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.five.AppConfig;
+import spring.five.MessageGenerator;
+import spring.five.NumberGenerator;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -18,6 +21,7 @@ public class Main {
         //get number generator bean from context (container)
         NumberGenerator numberGenerator
                 = context.getBean(NumberGenerator.class);
+
         //call method next() to get a random number
         int number = numberGenerator.next();
 
